0a9f2bd5e57a3d0563f80a9258c04bfacc1d6c9d
Antoine LUCAS <antoine.lucas@ynov.com>
2017-04-06 18:18:19 +0200

    /************************************************************************/ Qu'est ce qui a 32 dents et qui cache un monstre?

--- app/src/main/java/com/workshop/sucre/Produits.java
+++ app/src/main/java/com/workshop/sucre/Produits.java
@@ -68,10 +68,10 @@ public class Produits extends AppCompatActivity {
          */
         if (categorieDAO.selectionner(1) == null) {
 
-            Categorie mcdo_sandwichs = new Categorie(1, "Sandwichs", 1, "mcdo_nos_sandwichs");
-            Categorie mcdo_frites = new Categorie(2, "Frites", 1, "mcdo_frites_sauces");
-            Categorie mcdo_boissons = new Categorie(3, "Boissons", 1, "mcdo_nos_boissons");
-            Categorie mcdo_desserts = new Categorie(4, "Desserts", 1, "mcdo_nos_desserts");
+            Categorie mcdo_sandwichs = new Categorie(0, "Sandwichs", 1, "mcdo_nos_sandwichs");
+            Categorie mcdo_frites = new Categorie(1, "Frites", 1, "mcdo_frites_sauces");
+            Categorie mcdo_boissons = new Categorie(2, "Boissons", 1, "mcdo_nos_boissons");
+            Categorie mcdo_desserts = new Categorie(3, "Desserts", 1, "mcdo_nos_desserts");
 
             categorieDAO.ajouter(mcdo_sandwichs);
             categorieDAO.ajouter(mcdo_frites);
@@ -79,10 +79,10 @@ public class Produits extends AppCompatActivity {
             categorieDAO.ajouter(mcdo_desserts);
 
 
-            Categorie kfc_sandwichs = new Categorie(5, "Sandwichs", 2, "kfc_nos_sandwichs");
-            Categorie kfc_menu_enfant = new Categorie(6, "Enfant", 2, "kfc_menu_enfant");
-            Categorie kfc_boissons = new Categorie(7, "Boissons", 2, "kfc_nos_boissons");
-            Categorie kfc_desserts = new Categorie(8, "Desserts", 2, "kfc_nos_desserts");
+            Categorie kfc_sandwichs = new Categorie(4, "Sandwichs", 2, "kfc_nos_sandwichs");
+            Categorie kfc_menu_enfant = new Categorie(5, "Enfant", 2, "kfc_menu_enfant");
+            Categorie kfc_boissons = new Categorie(6, "Boissons", 2, "kfc_nos_boissons");
+            Categorie kfc_desserts = new Categorie(7, "Desserts", 2, "kfc_nos_desserts");
 
             categorieDAO.ajouter(kfc_sandwichs);
             categorieDAO.ajouter(kfc_menu_enfant);
@@ -96,50 +96,50 @@ public class Produits extends AppCompatActivity {
 
             /*--------------------------- mcdo_Sandwichs ---------------------------------------------------------------------------*/
 
-            Produit bigMac = new Produit(1, "BigMac", 1, 42, (float) 8.5, "mcdo_big_mac", 0, 1);
-            Produit royalBacon = new Produit(2, "RoyalBacon", 1, 34, 8, "mcdo_royal_bacon", 0, 1);
-            Produit royalCheese = new Produit(3, "RoyalCheese", 1, 37, (float) 9.5, "mcdo_royal_cheese", 0, 1);
-            Produit royalDeluxe = new Produit(4, "RoyalDeluxe", 1, 33, 7, "mcdo_royal_deluxe", 0, 1);
-            Produit tripleCheese = new Produit(5, "TripleCheese", 1, 32, 8, "mcdo_triple_cheese", 0, 1);
-            Produit premium = new Produit(6, "Premium", 1, 56, 13, "mcdo_premiumpoulet", 0, 1);
-            Produit mcwrapPoulet = new Produit(8, "McwrapPoulet", 1, 53, 4, "mcdo_mcwrap_poulet_bacon", 0, 1);
-            Produit mcFirstb = new Produit(9, "McFirstBoeuf", 1, 32, (float) 6.2, "mcdo_mcfirstboeuf", 0, 1);
-            Produit mcFirstp = new Produit(10, "McFirstPoisson", 1, 43, 7, "mcdo_mcfirstpoisson", 0, 1);
-            Produit mcwrapChevre = new Produit(11, "McwrapChevre", 1, 58, 4, "mcdo_mcwrap_chevre", 0, 1);
+            Produit bigMac = new Produit(1, "BigMac", 0, 42, (float) 8.5, "mcdo_big_mac", 0, 1);
+            Produit royalBacon = new Produit(2, "RoyalBacon", 0, 34, 8, "mcdo_royal_bacon", 0, 1);
+            Produit royalCheese = new Produit(3, "RoyalCheese", 0, 37, (float) 9.5, "mcdo_royal_cheese", 0, 1);
+            Produit royalDeluxe = new Produit(4, "RoyalDeluxe", 0, 33, 7, "mcdo_royal_deluxe", 0, 1);
+            Produit tripleCheese = new Produit(5, "TripleCheese", 0, 32, 8, "mcdo_triple_cheese", 0, 1);
+            Produit premium = new Produit(6, "Premium", 0, 56, 13, "mcdo_premiumpoulet", 0, 1);
+            Produit mcwrapPoulet = new Produit(8, "McwrapPoulet", 0, 53, 4, "mcdo_mcwrap_poulet_bacon", 0, 1);
+            Produit mcFirstb = new Produit(9, "McFirstBoeuf", 0, 32, (float) 6.2, "mcdo_mcfirstboeuf", 0, 1);
+            Produit mcFirstp = new Produit(10, "McFirstPoisson", 0, 43, 7, "mcdo_mcfirstpoisson", 0, 1);
+            Produit mcwrapChevre = new Produit(11, "McwrapChevre", 0, 58, 4, "mcdo_mcwrap_chevre", 0, 1);
 
 
             /*--------------------------- mcdo_frites ------------------------------------------------------------------------------*/
-            Produit barbecue = new Produit(12, "barbecue", 2, 27, 27, "barbecue", 0, 1);
-            Produit grande_deluxe = new Produit(13, "GrandeDeluxePotatoes", 2, 27, 27, "grande_deluxe", 0, 1);
-            Produit grande_frite = new Produit(14, "GrandeFrite", 2, 27, 27, "grande_frite", 0, 1);
-            Produit ketchup = new Produit(15, "Ketchup", 2, 27, 27, "ketchup", 0, 1);
-            Produit moutarde = new Produit(16, "Moutarde", 2, 27, 27, "moutarde", 0, 1);
-            Produit moy_deluxe = new Produit(17, "MoyennePotatoes", 2, 27, 27, "moy_deluxe", 0, 1);
-            Produit moy_frite = new Produit(18, "MoyenneFrite", 2, 27, 27, "moy_frite", 0, 1);
-            Produit sauce_chinoise = new Produit(19, "SauceChinoise", 2, 27, 27, "sauce_chinoise", 0, 1);
-            Produit sauce_curry = new Produit(20, "SauceCurry", 2, 27, 27, "sauce_curry", 0, 1);
-            Produit sauce_deluxe = new Produit(21, "SauceDeluxe", 2, 27, 27, "sauce_deluxe", 0, 1);
+            Produit barbecue = new Produit(12, "barbecue", 1, 27, 27, "barbecue", 0, 1);
+            Produit grande_deluxe = new Produit(13, "GrandeDeluxePotatoes", 1, 27, 27, "grande_deluxe", 0, 1);
+            Produit grande_frite = new Produit(14, "GrandeFrite", 1, 27, 27, "grande_frite", 0, 1);
+            Produit ketchup = new Produit(15, "Ketchup", 1, 27, 27, "ketchup", 0, 1);
+            Produit moutarde = new Produit(16, "Moutarde", 1, 27, 27, "moutarde", 0, 1);
+            Produit moy_deluxe = new Produit(17, "MoyennePotatoes", 1, 27, 27, "moy_deluxe", 0, 1);
+            Produit moy_frite = new Produit(18, "MoyenneFrite", 1, 27, 27, "moy_frite", 0, 1);
+            Produit sauce_chinoise = new Produit(19, "SauceChinoise", 1, 27, 27, "sauce_chinoise", 0, 1);
+            Produit sauce_curry = new Produit(20, "SauceCurry", 1, 27, 27, "sauce_curry", 0, 1);
+            Produit sauce_deluxe = new Produit(21, "SauceDeluxe", 1, 27, 27, "sauce_deluxe", 0, 1);
 
 
             /*--------------------------- mcdo_boissons ----------------------------------------------------------------------------*/
 
-            Produit mcdo_coca = new Produit(22, "CocaCola", 3, 27, 27, "mcdo_coca", 0, 1);
-            Produit mcdo_cocalight = new Produit(23, "CocaColaLight", 3, 27, 27, "mcdo_cocalight", 0, 1);
-            Produit mcdo_cocazero = new Produit(24, "CocaColaZero", 3, 27, 27, "mcdo_cocazero", 0, 1);
-            Produit mcdo_evian = new Produit(25, "Evian", 3, 27, 27, "mcdo_evian", 0, 1);
-            Produit mcdo_fanta = new Produit(26, "Fanta", 3, 27, 27, "mcdo_fanta", 0, 1);
-            Produit mcdo_icetea = new Produit(27, "IceTea", 3, 27, 27, "mcdo_icetea", 0, 1);
-            Produit mcdo_sprite = new Produit(28, "Sprite", 3, 27, 27, "mcdo_sprite", 0, 1);
-            Produit mcdo_spritezero = new Produit(29, "SpriteZero", 3, 27, 27, "mcdo_spritezero", 0, 1);
+            Produit mcdo_coca = new Produit(22, "CocaCola", 2, 27, 27, "mcdo_coca", 0, 1);
+            Produit mcdo_cocalight = new Produit(23, "CocaColaLight", 2, 27, 27, "mcdo_cocalight", 0, 1);
+            Produit mcdo_cocazero = new Produit(24, "CocaColaZero", 2, 27, 27, "mcdo_cocazero", 0, 1);
+            Produit mcdo_evian = new Produit(25, "Evian", 2, 27, 27, "mcdo_evian", 0, 1);
+            Produit mcdo_fanta = new Produit(26, "Fanta", 2, 27, 27, "mcdo_fanta", 0, 1);
+            Produit mcdo_icetea = new Produit(27, "IceTea", 2, 27, 27, "mcdo_icetea", 0, 1);
+            Produit mcdo_sprite = new Produit(28, "Sprite", 2, 27, 27, "mcdo_sprite", 0, 1);
+            Produit mcdo_spritezero = new Produit(29, "SpriteZero", 2, 27, 27, "mcdo_spritezero", 0, 1);
 
 
             /*--------------------------- mcdo_desserts ----------------------------------------------------------------------------*/
 
-            Produit ananas = new Produit(30, "Ananas", 4, 27, 27, "ananas", 0, 1);
-            Produit frappe = new Produit(31, "Frappe", 4, 27, 27, "frappe", 0, 1);
-            Produit mc_fleury = new Produit(32, "McFlurry", 4, 27, 27, "mc_fleurry", 0, 1);
-            Produit patisseries = new Produit(33, "Patisseries", 4, 27, 27, "patisseries", 0, 1);
-            Produit very_parfait = new Produit(34, "VeryParfait", 4, 27, 27, "very_parfait", 0, 1);
+            Produit ananas = new Produit(30, "Ananas", 3, 27, 27, "ananas", 0, 1);
+            Produit frappe = new Produit(31, "Frappe", 3, 27, 27, "frappe", 0, 1);
+            Produit mc_fleury = new Produit(32, "McFlurry", 3, 27, 27, "mc_fleurry", 0, 1);
+            Produit patisseries = new Produit(33, "Patisseries", 3, 27, 27, "patisseries", 0, 1);
+            Produit very_parfait = new Produit(34, "VeryParfait", 3, 27, 27, "very_parfait", 0, 1);
 
 
 
@@ -149,33 +149,33 @@ public class Produits extends AppCompatActivity {
 
             /*--------------------------- kfc_sandwichs ----------------------------------------------------------------------------*/
 
-            Produit boxMaster = new Produit(35, "BoxMaster", 5, 59, 1, "kfc_boxmaster", 0, 2);
-            Produit brazer = new Produit(36, "Brazer", 5, 37, 7, "kfc_brazer", 0, 2);
-            Produit doubleSweatFire = new Produit(37, "DoubleSweatFire", 5, (float) 51.6, 0, "kfc_double_sweatandfire", 0, 2);
-            Produit mixBucket = new Produit(38, "MixBucket", 5, (float) 22.5, 0, "kfc_mixbucket", 0, 2);
-            Produit saladeBrazer = new Produit(39, "SaladeBrazer", 5, (float) 5.5, 0, "kfc_salde_brazer", 0, 2);
-            Produit saladeCrispy = new Produit(40, "SaladeCrispy", 5, (float) 8.9, 0, "kfc_salade_crispy", 0, 2);
-            Produit tenders = new Produit(41, "Tenders", 5, 26, (float) 0.1, "kfc_tenders", 0, 2);
-            Produit theBoss = new Produit(42, "TheBoss", 5, (float) 41.7, 0, "kfc_theboss", 0, 2);
+            Produit boxMaster = new Produit(35, "BoxMaster", 4, 59, 1, "kfc_boxmaster", 0, 2);
+            Produit brazer = new Produit(36, "Brazer", 4, 37, 7, "kfc_brazer", 0, 2);
+            Produit doubleSweatFire = new Produit(37, "DoubleSweatFire", 4, (float) 51.6, 0, "kfc_double_sweatandfire", 0, 2);
+            Produit mixBucket = new Produit(38, "MixBucket", 4, (float) 22.5, 0, "kfc_mixbucket", 0, 2);
+            Produit saladeBrazer = new Produit(39, "SaladeBrazer", 4, (float) 5.5, 0, "kfc_salde_brazer", 0, 2);
+            Produit saladeCrispy = new Produit(40, "SaladeCrispy", 4, (float) 8.9, 0, "kfc_salade_crispy", 0, 2);
+            Produit tenders = new Produit(41, "Tenders", 4, 26, (float) 0.1, "kfc_tenders", 0, 2);
+            Produit theBoss = new Produit(42, "TheBoss", 4, (float) 41.7, 0, "kfc_theboss", 0, 2);
 
             /*--------------------------- kfc_frites -------------------------------------------------------------------------------*/
 
-            Produit kfcFrite = new Produit(43, "Frite", 6, 35, 1, "kfc_frites", 0, 2);
+            Produit kfcFrite = new Produit(43, "Frite", 5, 35, 1, "kfc_frites", 0, 2);
 
             /*--------------------------- kfc_boissons -----------------------------------------------------------------------------*/
 
-            Produit kfc_coca = new Produit(44, "CocaCola", 7, 27, 27, "mcdo_coca", 0, 2);
-            Produit kfc_cocalight = new Produit(45, "CocaColaLight", 7, 27, 27, "mcdo_cocalight", 0, 2);
-            Produit kfc_cocazero = new Produit(46, "CocaColaZero", 7, 27, 27, "mcdo_cocazero", 0, 2);
-            Produit kfc_evian = new Produit(47, "Evian", 7, 27, 27, "mcdo_evian", 0, 2);
-            Produit kfc_fanta = new Produit(48, "Fanta", 7, 27, 27, "mcdo_fanta", 0, 2);
-            Produit kfc_icetea = new Produit(49, "IceTea", 7, 27, 27, "mcdo_icetea", 0, 2);
-            Produit kfc_sprite = new Produit(50, "Sprite", 7, 27, 27, "mcdo_sprite", 0, 2);
-            Produit kfc_spritezero = new Produit(60, "SpriteZero", 7, 27, 27, "mcdo_spritezero", 0, 2);
+            Produit kfc_coca = new Produit(44, "CocaCola", 6, 27, 27, "mcdo_coca", 0, 2);
+            Produit kfc_cocalight = new Produit(45, "CocaColaLight", 6, 27, 27, "mcdo_cocalight", 0, 2);
+            Produit kfc_cocazero = new Produit(46, "CocaColaZero", 6, 27, 27, "mcdo_cocazero", 0, 2);
+            Produit kfc_evian = new Produit(47, "Evian", 6, 27, 27, "mcdo_evian", 0, 2);
+            Produit kfc_fanta = new Produit(48, "Fanta", 6, 27, 27, "mcdo_fanta", 0, 2);
+            Produit kfc_icetea = new Produit(49, "IceTea", 6, 27, 27, "mcdo_icetea", 0, 2);
+            Produit kfc_sprite = new Produit(50, "Sprite", 6, 27, 27, "mcdo_sprite", 0, 2);
+            Produit kfc_spritezero = new Produit(60, "SpriteZero", 6, 27, 27, "mcdo_spritezero", 0, 2);
 
             /*--------------------------- kfc_desserts -----------------------------------------------------------------------------*/
 
-            Produit kreamBall = new Produit(61, "KreamBall", 8, 50, 50, "kfc_ kreamball", 0, 2);
+            Produit kreamBall = new Produit(61, "KreamBall", 7, 50, 50, "kfc_ kreamball", 0, 2);
 
 
             /*-----------------------------DAO McDo --------------------------------------------------------------------------------*/
